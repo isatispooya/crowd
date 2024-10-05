d5d9cb85af4a5ce11e88b7d1d09ffe08db2c46f4
Mahya Abouei <mahyaaboueii@gmail.com>
2024-10-05 09:06:39 +0330

    reports app

---
+++ reports/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.views import ParticipationReportViewset,AuditReportViewset,ProgressReportViewset
+
+
+urlpatterns = [
+    path('progres/report/admin/<str:trace_code>/', ProgressReportViewset.as_view(), name='progres-report-admin'),
+    path('audit/report/admin/<str:trace_code>/', AuditReportViewset.as_view(), name='audit-report-admin'),
+    path('participation/report/<str:trace_code>/', ParticipationReportViewset.as_view(), name='get-participation-report-user'),
+]
