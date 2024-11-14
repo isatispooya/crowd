5075f5b7ac0d5ba05527c4d4cca885e54b083511
moeendehqan <moeen.dehqan@gmail.com>
2024-11-14 09:56:37 +0330

    .

--- plan/views.py
+++ plan/views.py
@@ -609,8 +609,7 @@ class PaymentDocument(APIView):
         amount = int(request.data.get('amount')) # سهم درخواستی کاربر 
         amount_collected_now = information_plan.amount_collected_now # مبلغ جمه اوری شده تا به  الان
         plan_total_price = plan.total_units # کل سهم قابل عرضه برای طرح 
-        purchaseable_amount = int(plan_total_price - amount_collected_now) # مبلغ قابل خرید همه کاربران 
-        print('purchaseable_amount',purchaseable_amount,plan_total_price , amount_collected_now)
+        purchaseable_amount = int(int(plan_total_price*10000) - amount_collected_now) # مبلغ قابل خرید همه کاربران 
         if amount > purchaseable_amount :
             return Response({'error': 'مبلغ بیشتر از سهم قابل خرید است'}, status=status.HTTP_400_BAD_REQUEST)
         
@@ -627,8 +626,9 @@ class PaymentDocument(APIView):
                   
                 
         if legal_user == False :
-            amount_personal_min = plan.real_person_minimum_availabe_price  #حداقل سهم قابل خرید حقیقی
-            amount_personal_max = plan.real_person_maximum_available_price #حداکثر سهم قابل خرید حقیقی
+            amount_personal_min = int(plan.real_person_minimum_availabe_price/10000)  #حداقل سهم قابل خرید حقیقی
+            amount_personal_max = int(plan.real_person_maximum_available_price/10000) #حداکثر سهم قابل خرید حقیقی
+            print('amount_personal_min',amount_personal_min , 'amount_personal_max',amount_personal_max,'amount',amount)
             if amount_personal_min is not None and amount_personal_max is not None:
                 if amount < amount_personal_min or amount > amount_personal_max :
                     return Response({'error': 'مبلغ بیشتر یا کمتر از  حد مجاز قرارداد شده است'}, status=status.HTTP_400_BAD_REQUEST)
